10871fd407e12e49e050182db47d26afbc3af7c5
joshkeselman <jhkeselman@wpi.edu>
2024-11-04 19:44:27 -0500

    Initial commit

---
+++ control/pid_controller.m
@@ -0,0 +1,37 @@
+function [F, M, trpy, drpy] = pid_controller(qd, t, qn, params)
+% CONTROLLER quadrotor controller
+% The current states are:
+% qd{qn}.pos, qd{qn}.vel, qd{qn}.euler = [roll;pitch;yaw], qd{qn}.omega
+% The desired states are:
+% qd{qn}.pos_des, qd{qn}.vel_des, qd{qn}.acc_des, qd{qn}.yaw_des, qd{qn}.yawdot_des
+% Using these current and desired states, you have to compute the desired controls
+
+% =================== Your code goes here ===================
+
+persistent gd;
+persistent icnt;
+ if isempty(gd)
+     gd = zeros(0,3);
+     icnt = 0;
+ end
+ icnt = icnt + 1;
+ 
+% =================== Your code starts here ===================
+%% Parameter Initialization
+    kp_roll = 0;
+    kd_roll = 0;
+    kp_pitch = 0;
+    kp_pitch = 0;
+    kp_yaw = 0;
+    kd_yaw = 0;
+
+    F = 0;
+    M = [0;0;0];
+
+% =================== Your code ends here ===================
+
+%Output trpy and drpy as in hardware
+trpy = [0, 0, 0, 0];
+drpy = [0, 0, 0, 0];
+
+end
